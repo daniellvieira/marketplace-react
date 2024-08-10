1f33418f1d91cae7480d2d85b3137cbdcfb90b62
Daniel Vieira <danielvieiraufc@gmail.com>
2024-08-10 11:41:40 -0300

    feat: add toast in categories and items

--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom/client';
 import store from './store';
 import { Provider } from 'react-redux';
 import Router from './routes';
+import { createStandaloneToast } from '@chakra-ui/react';
+
 import './index.css';
+
+const { ToastContainer } = createStandaloneToast();
 
 // As of React 18
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
   // <React.StrictMode>
   <Provider store={store}>
     <Router />
+    <ToastContainer />
   </Provider>,
   // </React.StrictMode>,
 );
--- src/store/reducers/categories.js
+++ src/store/reducers/categories.js
@@ -1,5 +1,8 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import categoriesService from 'services/categories';
+import { createStandaloneToast } from '@chakra-ui/react';
+
+const { toast } = createStandaloneToast();
 
 // https://redux-toolkit.js.org/api/createAsyncThunk
 export const getCategories = createAsyncThunk(
@@ -16,9 +19,37 @@ export const categoriesSlice = createSlice({
     },
   },
   extraReducers: (builder) => {
-    builder.addCase(getCategories.fulfilled, (_state, { payload }) => {
-      return payload;
-    });
+    const builderReducer = builder
+      .addCase(getCategories.fulfilled, (_state, { payload }) => {
+        toast({
+          title: 'Categories loaded.',
+          description: "We've loaded all categories.",
+          status: 'success',
+          duration: 2000,
+          isClosable: true,
+        });
+
+        return payload;
+      })
+      .addCase(getCategories.pending, (_state, { payload }) => {
+        toast({
+          title: 'Loading categories.',
+          description: "We're loading you categories.",
+          status: 'loading',
+          duration: 2000,
+          isClosable: true,
+        });
+      })
+      .addCase(getCategories.rejected, (_state, { payload }) => {
+        toast({
+          title: 'An error occurred.',
+          description: 'Unable to loaded categories.',
+          status: 'error',
+          duration: 2000,
+          isClosable: true,
+        });
+      });
+    return builderReducer;
   },
 });
 
