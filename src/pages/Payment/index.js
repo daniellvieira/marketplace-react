95358b901d948b4ca5a8917217e8bf8e28c5f466
Daniel Vieira <danielvieiraufc@gmail.com>
2024-08-24 10:38:08 -0300

    feat: add more informations in checkotu

--- src/pages/Payment/index.js
+++ src/pages/Payment/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Header from 'components/Header';
 import Select from 'components/Select';
@@ -9,10 +9,19 @@ import { useDispatch, useSelector } from 'react-redux';
 import { LOAD_CHECKOUT_REQUEST } from 'store/cart/reducer';
 
 const Payment = () => {
+  const [paymentMethod, setPaymentMethod] = useState('-');
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user);
   const total = useSelector((state) => state.cart.total);
-  console.log(total);
+  const tax = paymentMethod === '-' ? 0 : total * (paymentMethod.tax - 1);
+  const totalWithTax = paymentMethod === '-' ? total : total + tax;
+
+  const changePaymentMethod = (event) => {
+    const cardId = event.target.value;
+    if (cardId === '-') return setPaymentMethod('-');
+
+    setPaymentMethod(user.cards.find((card) => card.id === cardId));
+  };
 
   useEffect(() => {
     dispatch(LOAD_CHECKOUT_REQUEST());
@@ -25,7 +34,12 @@ const Payment = () => {
         <p className={S.paymentMethod}>
           Hi {user.name}! Choice your payment method:{' '}
         </p>
-        <Select placeholder="Payment method" alt="payment method">
+        <Select
+          placeholder="Payment method"
+          alt="payment method"
+          value={paymentMethod.id}
+          onChange={changePaymentMethod}
+        >
           <option value="-">---- Payment Method ----</option>
           {user.cards?.map((card) => (
             <option key={card.id} value={card.id}>
@@ -34,7 +48,20 @@ const Payment = () => {
           ))}
         </Select>
         <div className={S.content}>
-          <p>Total + Tax: R$ {total.toFixed(2)}</p>
+          {paymentMethod !== '-' && (
+            <>
+              <p>
+                Payment Method {paymentMethod.name} has a tax rate of{' '}
+                {paymentMethod.tax}x{' '}
+              </p>
+              <p>
+                The balance of this card is R${' '}
+                {paymentMethod.balance.toFixed(2)}
+              </p>
+              <p>Tax: R$ {tax.toFixed(2)}</p>
+            </>
+          )}
+          <p>Total + Tax: R$ {totalWithTax.toFixed(2)}</p>
         </div>
         <div className={S.finish}>
           <Button>Finish</Button>
